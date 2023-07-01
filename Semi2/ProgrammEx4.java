a05589f00bc2cf2877d6676068f56256997312c4
Ivan <csgo.dr@mail.ru>
2023-07-01 19:15:50 +0700

    ex4 was completed

---
+++ Semi2/ProgrammEx4.java
@@ -0,0 +1,31 @@
+package Semi2;
+
+import java.util.Scanner;
+
+public class ProgrammEx4 {
+    public static void main(String[] args) {
+        enterString();
+    }
+    public static void enterString(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter the string");
+            String s = scanner.nextLine();
+            if (s.isEmpty()){
+                throw new EmptyStringException("String is empty, fix it");
+            } else {
+                System.out.printf("Your enter '%s'", s);
+            }
+        } catch (EmptyStringException e){
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
+class EmptyStringException extends Exception{
+    public EmptyStringException(String message) {
+        super(message);
+    }
+}
+
